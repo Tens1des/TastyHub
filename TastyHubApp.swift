eb0556f9cc451b2a288bbf741c70dfdd138357f6
Roman <Romankotov3213@gmail.com>
2024-12-18 01:01:29 +0300

    Add structure and fireBase

---
+++ TastyHubApp.swift
@@ -0,0 +1,23 @@
+//
+//  TastyHubApp.swift
+//  TastyHub
+//
+//  Created by Роман  on 11.12.2024.
+//
+import SwiftUI
+import Firebase
+
+@main
+struct TastyHubApp: App {
+    init() {
+        
+        
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
